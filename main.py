32373ea8e3b9a3a0f30ccd60c97383acd9106130
Bhargav Muktevi <durgabhargav003@gmail.com>
2022-07-11 21:46:42 +0200

    final commit

--- main.py
+++ main.py
@@ -12,11 +12,17 @@ def do_stuff_on_page_load():
 @st.cache
 def get_data(path: str) -> pd.DataFrame:
     # Fetches the data from the given url and returns a dataframe
+
     # url = "https://opendata.ecdc.europa.eu/covid19/nationalcasedeath_eueea_daily_ei/json/"
     # response = requests.get(url)
     # dictr = response.json()
     # records = dictr['records']
-    # dataframe = json_normalize(records)
+    # df = json_normalize(records)
+    # df['cases'] = df['cases'].abs()
+    # df['deaths'] = df['deaths'].abs()
+    # df = df.dropna().reset_index(drop=True)
+    # df.to_csv("corona_data_2.csv")
+
     dataframe = pd.read_csv(path, index_col=0)
     dataframe['dateRep'] = pd.to_datetime(dataframe['dateRep'], format="%d/%m/%Y")
     dataframe["popData2020"] = dataframe["popData2020"].astype(float)
@@ -27,7 +33,7 @@ do_stuff_on_page_load()
 st.sidebar.title(":mask: Dashboard :mask:")
 st.sidebar.markdown("##")
 
-file_path = "../streamlit_corona/corona_data.csv"
+file_path = "../streamlit_corona/corona_data_2.csv"
 df = get_data(file_path)
 
 plot_type = st.sidebar.radio(
